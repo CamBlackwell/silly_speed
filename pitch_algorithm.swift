aba31bec8f61d1c6e9c27a8335fb2142ade8af80
CamBlackwell <cameronBlackwell89@gmail.com>
2025-12-15 12:18:57 +1030

    feat: added PitchALgorithm enum file

---
+++ pitch_algorithm.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum PitchAlgorithm: String, CaseIterable, Codeable {
+    case apple = "Apple (Default)"
+    case rubberBand = "Rubber Band (Best quality for extreme changes)"
+    case soundTouch = "SoundTouch (Balanced quality and performance)"
+    case signalSmith = "SignalSmith (Modern)"
+    
+    var isImplemented: Bool {
+        switch self {
+        case .apple:
+            return true
+        case .rubberBand, .soundTouch, .signalSmith:
+            return false
+        }
+    }
+}
