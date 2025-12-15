27b0b1ab8f7b9acad3f0067831fa51ad6b578d16
CamBlackwell <cameronBlackwell89@gmail.com>
2025-12-15 12:25:27 +1030

    feat: added audio Engine protocol

---
+++ audio_engine_protocol.swift
@@ -0,0 +1,18 @@
+import Foundation
+import AVFoundation
+
+
+protocol AudioEngineProtocol {
+    var isPlaying: Bool { get }
+    var currentTime: TimeInterval { get }
+    var duration: TimeInterval { get }
+    
+    func load(audioFile: AudioFile)
+    func play()
+    func pause()
+    func stop()
+    func seek(to time: TimeInterval)
+    func setVolume(_ volume: Float)
+    func setTempo (_ tempo: Float)
+    func setPitch (_ pitch: Float)
+}
